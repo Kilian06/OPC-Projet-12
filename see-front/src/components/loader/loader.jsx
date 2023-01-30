3d2cd0d03de38f92be332776d4a685d308131424
SiouuL <kilian.leroy@me.com>
2023-01-30 13:20:31 +0100

    VF add Loader

---
+++ see-front/src/components/loader/loader.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+/**
+ * 
+ * @returns a spinner, it's appear during the time to get data from fetch
+ */
+function Spinner() {
+    return (
+<>
+<div className="loader"></div>
+</>
+    );
+}
+
+export default Spinner;
